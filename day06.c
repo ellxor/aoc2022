cef13b4b628f34103c235480ea94b29e8fcdd1ea
ellxor <ellxor@protonmail.ch>
2022-12-06 19:06:34 +0000

    Add day 06 solution

---
+++ day06.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+
+#include "day06input.h"
+
+unsigned solve(size_t N)
+{
+	const char *buffer = input;
+	uint32_t prev = 0;
+	
+	for (int i = 0; i < N; i++)
+		prev ^= 1 << (*buffer++ - 'a');
+
+	while (*buffer && __builtin_popcount(prev) != N)
+	{
+		prev ^= 1 << (*buffer - 'a');
+		prev ^= 1 << (*(buffer- N) - 'a');
+		++buffer;
+	}
+
+	return buffer - input;
+}
+
+int main()
+{
+	clock_t t1 = clock();
+
+	unsigned part1 = solve(4);
+	unsigned part2 = solve(14);
+
+	clock_t t2 = clock();
+
+	printf("%u\n", part1);
+	printf("%u\n", part2);
+
+	printf("Time: %ld\n", t2 - t1);
+}
--- run.sh
+++ run.sh
@@ -7,5 +7,6 @@ clang $CFLAGS day02.c && ./a.out
 clang $CFLAGS day03.c && ./a.out
 clang $CFLAGS day04.c && ./a.out
 clang $CFLAGS day05.c && ./a.out
+clang $CFLAGS day06.c && ./a.out
 
 rm -rf a.out
